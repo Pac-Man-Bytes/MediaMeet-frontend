2a087dbe0cb2efc38003521952fb7f1d35a2df5d
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2020-09-13 04:22:52 -0500

    Installed fxLayou

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MediaObserver,MediaChange} from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
+import { filter, map} from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit,OnDestroy {
   title = 'MediaMeet-frontend';
-}
+  mediaSub:Subscription;
+  deviceXs: boolean;
+
+  constructor(public mediaObserver:MediaObserver){}
+  ngOnInit(){
+    this.mediaSub = this.mediaObserver.asObservable().pipe(
+      filter((results: MediaChange[]) => results.length > 0),
+      map((results: MediaChange[]) => results[0])
+    ).subscribe(
+      (result: MediaChange) =>{
+        console.log(result.mqAlias);
+        this.deviceXs = result.mqAlias === 'xs' ? true:false;
+
+      }
+    )
+  }
+  ngOnDestroy(){
+    this.mediaSub.unsubscribe();
+  }
+  
+}    
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
