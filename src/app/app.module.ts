650792e2713e5d3b3b6a399e1b7be6bf139c0776
santiago <srubianof>
2020-09-25 01:24:40 -0500

    cositas varias :3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,33 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChatComponent } from './chat/chat.component';
 import { MainComponent } from './main/main.component';
 import {MediaService} from './services/media-service.service';
+import { LoginComponent } from './login/login.component';
+import {RouterModule, Routes} from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+import {FormsModule} from '@angular/forms';
+const routes: Routes = [
+  {path: 'user/login', component: LoginComponent},
+  {path: 'user/register', component: RegisterComponent},
+  {path: 'player', component: PlayerComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
     PlayerComponent,
     ChatComponent,
     MainComponent,
+    LoginComponent,
+    NavbarComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [MediaService],
   bootstrap: [AppComponent]
