b3a8cf986cfe4c1881a66e331ec43026ebe34574
Davor Cortés <davorcortes.dc@gmail.com>
2020-10-05 13:01:05 -0500

    form view

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {RoomService} from '../../services/room.service';
+import {Room} from '../../clases/room';
+import swal from 'sweetalert2';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  room: Room;
+  constructor(private roomService: RoomService, private router:Router) {
+    this.room = new Room();
+  }
 
   ngOnInit(): void {
+  }
+
+  createRoom(): void{
+    this.roomService.createRoom(this.room).subscribe(resp => {
+      swal.fire('Sala creada', resp.id, 'success');
+      this.room.id = resp.id;
+    });
+    this.router.navigate([`rooms/${this.room.id}`]);
   }
 
 }
