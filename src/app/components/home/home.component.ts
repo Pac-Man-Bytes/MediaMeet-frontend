8686a0208e4e8f3ebcbe333fa9af1aaa4cb406d8
srubianof <srubianof@icloud.com>
2020-10-15 15:24:25 -0500

    login redesign
    missing register

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RoomService} from '../../services/room.service';
 import {Room} from '../../clases/room';
 import swal from 'sweetalert2';
 import {Router} from '@angular/router';
+import {AngularFireAuth} from '@angular/fire/auth';
+import Swal from 'sweetalert2';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-home',
@@ -13,34 +16,42 @@ export class HomeComponent implements OnInit {
   room: Room;
   cRoom: Room;
   errorGet: string;
-  constructor(private roomService: RoomService, private router: Router) {
+  UDI: string;
+  UName: string;
+
+  constructor(private roomService: RoomService, private router: Router, private oAuth: AngularFireAuth) {
     this.room = new Room();
     this.cRoom = new Room();
   }
 
   ngOnInit(): void {
-  }
-  redirectToRoom(roomId): void{
-    this.router.navigate([ '/room', roomId]);
+    this.getUID();
   }
 
-  createRoom(): void{
+  redirectToRoom(roomId): void {
+    this.router.navigate(['/room', roomId]);
+  }
+
+  createRoom(): void {
     this.roomService.createRoom(this.cRoom).subscribe(resp => {
       swal.fire('Sala creada', resp.id, 'success');
       this.cRoom.id = resp.id;
       this.redirectToRoom(this.cRoom.id);
     });
   }
-  getRoom(): void{
+
+  getRoom(): void {
     this.roomService.getRoom(this.room.id).subscribe(resp => {
-      swal.fire('Te has unido a', resp.name, 'success');
-      this.redirectToRoom(this.room.id);
-    },
-    error => {
-      console.log(error);
-      this.errorGet = error.error.message as string;
-    }
+        swal.fire('Te has unido a', resp.name, 'success');
+        this.redirectToRoom(this.room.id);
+      },
+      error => {
+        console.log(error);
+        this.errorGet = error.error.message as string;
+      }
     );
   }
-
+  getUID(): void {
+    console.log(firebase.auth().currentUser.uid);
+  }
 }
