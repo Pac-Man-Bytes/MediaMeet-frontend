b883e3ae7d0fbc94055327cf756eb18e81489c1b
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2020-10-15 20:58:40 -0500

    gitflow-feature-stash: roomUi

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {PlayerComponent} from "../player/player.component";
 
 @Component({
   selector: 'app-room',
@@ -8,6 +9,7 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class RoomComponent implements OnInit {
   public roomId;
+  @ViewChild(PlayerComponent, {static: true}) player: PlayerComponent;
   constructor(private route: ActivatedRoute) {
     this.route.params.subscribe(params => {
       this.roomId = params['roomId'];
